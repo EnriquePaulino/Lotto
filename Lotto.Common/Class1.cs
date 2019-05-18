b9d8a4f18b7489ee6cc65d288915170292dfb084
EnriquePaulino <paulinoenrique@gmail.com>
2019-05-18 13:43:10 -0400

    Crear Soluciones Lotto

---
+++ Lotto.Common/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Lotto.Common
+{
+    public class Class1
+    {
+    }
+}
---
+++ Lotto.UIClassic/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Lotto.UIClassic
+{
+    public class Class1
+    {
+    }
+}
